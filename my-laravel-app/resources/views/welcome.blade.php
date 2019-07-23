a2efd827facbf97aa96ad9258791a2c157fbf762
Takafumi Moriyama <moriyama@TakafuminoMacBook-Pro.local>
2019-07-23 21:30:58 +0900

    welcome画面の修正

--- my-laravel-app/resources/views/welcome.blade.php
+++ my-laravel-app/resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="flex-center position-ref full-height">
         <div class="content">
-            <p>WELCOME TO BookMark</p>
+            <p>WELCOME To BookMark</p>
             <a href='{{ route('books.index') }}'>はじめる</a>
         </div>
 </div>
