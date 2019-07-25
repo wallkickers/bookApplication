ea7bdd8ee55e1adcfd594bc1b336c7dc76001ec7
Takafumi Moriyama <moriyama@TakafuminoMacBook-Pro.local>
2019-07-25 21:38:42 +0900

    マイページ 検索機能実装

--- my-laravel-app/app/Http/Controllers/BookController.php
+++ my-laravel-app/app/Http/Controllers/BookController.php
@@ -8,9 +8,27 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Book;
 use PhpOffice\PhpSpreadsheet\Reader\Xlsx as Reader;
+use Illuminate\Support\Facades\Redirect;
 
 class BookController extends Controller
 {
+    public function search(Request $request)
+    {
+        if(isset($request->keyword)){
+            $keyword = $request->keyword;
+            $query = Book::query();
+            $searchedBooks = $query
+                ->where('title_kana', 'LIKE', "%".$keyword."%")
+                ->get();
+
+            return view('book.home')->with([
+                'books' => $searchedBooks
+            ]);
+        }else{
+            Redirect::route('books.index');
+        }
+    }
+
     public function index()
     {
         $books = Book::all();
