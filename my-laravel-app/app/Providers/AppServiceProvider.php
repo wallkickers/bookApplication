38ec3eff229c00893ead12d42b25573e8f5a2384
Takafumi Moriyama <moriyama@TakafuminoMacBook-Pro.local>
2019-07-26 22:13:47 +0900

    本番環境の設定をAppServiceProviderに追加

--- my-laravel-app/app/Providers/AppServiceProvider.php
+++ my-laravel-app/app/Providers/AppServiceProvider.php
@@ -25,6 +25,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // 本番環境(Heroku)でhttpsを強制する
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
     }
 }
