9c5b1416c3e796721f50092f605692c8ab8871f7
Takafumi Moriyama <moriyama@TakafuminoMacBook-Pro.local>
2019-08-08 22:18:47 +0900

    バグ修正 slack通知をコメントアウト

--- my-laravel-app/app/Http/Controllers/ApplicationController.php
+++ my-laravel-app/app/Http/Controllers/ApplicationController.php
@@ -24,9 +24,6 @@ class ApplicationController extends Controller
 
     public function store(Request $request)
     {
-        $startTime = microtime(true);
-        $initialMemory = memory_get_usage();
-
         // 何かの処理
         $user = Auth::user();
         $bookId = $request->book;
@@ -34,21 +31,7 @@ class ApplicationController extends Controller
         $book->user_id = $user->id;
         $book->save();
 
-        $initialMemory2 = memory_get_usage();
-
         // \Slack::send('貸し出し申請がありました。名前：'.$user->name.' 書籍名：'.$book->book_name.'：URL:'.env('APP_URL').'/books/'.$book->id);
-
-        $runningTime =  microtime(true) - $startTime;
-        $usedMemory = (memory_get_peak_usage() - $initialMemory) / (1024 * 1024);
-        $usedMemory2 = (memory_get_peak_usage() - $initialMemory2) / (1024 * 1024);
-
-        var_dump('running time: ' . $runningTime . ' [s]'); // or var_dump()
-        var_dump('used memory: ' . $usedMemory . ' [MB]'); // or var_dump()
-        var_dump('used slack memory: ' . $usedMemory2 . ' [MB]'); // or var_dump()
-
-        \Log::debug('used memory: ' . $usedMemory . ' [MB]');
-        \Log::debug('used slack memory: ' . $usedMemory2 . ' [MB]');
-
         return view('application.complete');
     }
 
@@ -59,7 +42,7 @@ class ApplicationController extends Controller
         $book = Book::where('id', $bookId)->first();
         $book->user_id = null;
         $book->save();
-        \Slack::send('本が返却されました。名前：'.$user->name.' 書籍名：'.$book->book_name.'：URL:'.env('APP_URL').'/books/'.$book->id);
+        // \Slack::send('本が返却されました。名前：'.$user->name.' 書籍名：'.$book->book_name.'：URL:'.env('APP_URL').'/books/'.$book->id);
         return view('application.complete');
     }    
 }
