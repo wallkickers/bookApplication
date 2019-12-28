296d1f914dbd3a658debbf9a5013b9a041317000
Takafumi Moriyama <taka.youthful.426@gmail.com>
2019-12-28 15:38:19 +0900

    管理_通知設定更新処理追加

---
+++ web/app/Http/Controllers/Admin/SlackController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+// require 'vendor/autoload.php';
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use App\Slack;
+
+class SlackController extends Controller
+{
+    public function index()
+    {
+        $slack = slack::find(1);
+        return view('admin.slack.slack')
+        ->with([
+            'slack' => $slack,
+            'message' => '',
+        ]);
+    }
+
+    public function update(Request $request)
+    {
+        $slack = slack::find(1);
+        $slack->update($request->all());
+
+        return view('admin.slack.slack')
+        ->with([
+            'slack' => $slack,
+            'message' => "更新が完了しました。",
+        ]);
+    }
+}
