195e2ed1d071419bf6e16c1c8c21e6435af8100d
Takafumi Moriyama <taka.youthful.426@gmail.com>
2020-01-02 10:29:57 +0900

    ユーザー_書籍申請メッセージ追加

--- web/resources/views/application/complete.blade.php
+++ web/resources/views/application/complete.blade.php
@@ -5,9 +5,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">書籍貸し出し</div>
+                <div class="card-header">書籍申請</div>
                 <div class="card-body center">
-                    <p>貸し出し完了</p>
+                    <p>{{ $message }}</p>
                     <a href='{{ route('books.index') }}' class='btn-sticky'>書籍一覧へ戻る</a>
                 </div>
             </div>
