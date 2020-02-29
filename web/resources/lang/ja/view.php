9ffa6d65ccad4b406795e8d3efb0b37ff98f7e92
Takafumi Moriyama <taka.youthful.426@gmail.com>
2020-02-29 10:54:19 +0900

    ユーザー_会員登録画面のテキストをlangへ記述

--- web/resources/lang/ja/view.php
+++ web/resources/lang/ja/view.php
@@ -6,4 +6,8 @@ return [
     'Register' => '会員登録',
     'MyPage' => 'マイページ',
     'ImportBook' => '書籍登録',
+    'Name' => '名前',
+    'E-Mail' => 'メールアドレス',
+    'Password' => 'パスワード',
+    'ConfirmPassword' => 'パスワード(確認)',
 ];
