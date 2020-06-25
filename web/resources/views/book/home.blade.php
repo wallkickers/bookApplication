726bc2a1bd3ae74f4c49c584d5ae664e0b1fe727
Takafumi Moriyama <taka.youthful.426@gmail.com>
2020-06-25 21:00:35 +0900

    書籍一覧_検索フォームを中央へ配置。

--- web/resources/views/book/home.blade.php
+++ web/resources/views/book/home.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
 <div class="container">
+    <div class="search-form">
+        <form method="GET" action="{{ route('books.search') }}">
+            @csrf
+            <div class="form-group　search_container">
+                <input type="text" name="keyword" size="25" @isset($keyword) value="{{$keyword}}" @endisset class="form search-input" placeholder="カタカナ検索">
+                <button type="submit" class="search-button"><i class="fas fa-search"></i></button>
+            </div>
+        </form>
+    </div>
     <div class="row justify-content-center">
         <div class="col-md-8">
             @if (session('status'))
@@ -10,13 +19,6 @@
             </div>
             @endif
 
-            <form method="GET" action="{{ route('books.search') }}">
-                @csrf
-                <div class="form-group">
-                    <input type="text" name="keyword" @isset($keyword) value="{{$keyword}}" @endisset class="form" placeholder="カタカナ検索">
-                    <button type="submit" value="検索" class="">検索</button>
-                </div>
-            </form>
             <div class="fh5co-narrow-content">
                 <div class="row animate-box" data-animate-effect="fadeInLeft">
                     @foreach ($books as $book)
