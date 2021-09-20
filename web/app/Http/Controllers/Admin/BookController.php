f8361e5226da10c62501e31167f38321e0cc0f78
Takafumi Moriyama <taka.youthful.426@gmail.com>
2021-09-20 20:27:30 +0900

    リファクタ_admin_BookControllerに関するファイルをメンテナンス

--- web/app/Http/Controllers/Admin/BookController.php
+++ web/app/Http/Controllers/Admin/BookController.php
@@ -1,13 +1,13 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\Admin;
 
-// require 'vendor/autoload.php';
-
+use App\Http\Controllers\Controller;
+use App\Services\BookService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\Controller;
-use App\Services\Admin\BookService;
 
 class BookController extends Controller
 {
@@ -18,37 +18,43 @@ class BookController extends Controller
         $this->bookService = $bookService;
     }
 
+    /**
+     * 書籍一覧
+     */
     public function index()
     {
         $books = $this->bookService
             ->getAllBooksOrderByIdAsc()
             ->paginate(config('app.pagesize'));
-        return view('admin.book.index')->with([
-            'books' => $books
-        ]);
+        return view('admin.book.index', compact('books'));
     }
 
+    /**
+     * 書籍詳細
+     */
     public function show(Request $request)
     {
         $user = Auth::user();
         $bookId = $request->book;
-        $book = $this->bookService
-            ->findByBookId($bookId);
+        $book = $this->bookService->findByBookId($bookId);
 
-        return view('admin.book.show', [
-            'user' => $user,
-            'book' => $book
-        ]);
+        return view('admin.book.show', compact('user', 'book'));
     }
 
+    /**
+     * 書籍削除
+     */
     public function destroy(Request $request)
     {
         $bookId = $request->book;
-        $result = $this->bookService->deleteByBookId($bookId);
+        $this->bookService->deleteByBookId($bookId);
 
         return redirect()->route('admin.books.index');
     }
 
+    /**
+     * 書籍履歴
+     */
     public function history(Request $request)
     {
         $display_item = $request->display_item;
@@ -59,13 +65,8 @@ class BookController extends Controller
             $rental_histories = $this->bookService
                 ->getBookHistoriesWithDisplayItemOrderByIdAsc($request->display_item);
         }
-
         $rental_histories = $rental_histories->paginate(config('app.pagesize'));
 
-        return view('admin.rental_histories.index')
-            ->with([
-                'rental_histories' => $rental_histories,
-                'display_item' => $display_item,
-            ]);
+        return view('admin.rental_histories.index', compact('rental_histories', 'display_item'));
     }
 }
