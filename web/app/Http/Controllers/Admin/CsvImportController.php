c8210d8199af4e5181c0c2cb41348cc90f93cb0c
Takafumi Moriyama <taka.youthful.426@gmail.com>
2021-09-24 22:39:51 +0900

    リファクタ_admin_csvControllerに関するファイルをメンテナンス

--- web/app/Http/Controllers/Admin/CsvImportController.php
+++ web/app/Http/Controllers/Admin/CsvImportController.php
@@ -1,71 +1,45 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Book;
 use App\Http\Requests\StoreRequest;
+use App\Services\BookService;
+use App\Services\CsvImportService;
 
 class CsvImportController extends Controller
 {
+    public function __construct(
+        CsvImportService $csvImportService,
+        BookService $bookService
+    ){
+        $this->csvImportService = $csvImportService;
+        $this->bookService = $bookService;
+    }
+
+    /**
+     * CSVインポート画面
+     */
     public function create()
     {
         return view('admin.form');
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * CSVデータ処理
+     * @param  StoreRequest $request
      */
     public function store(StoreRequest $request)
     {
-        setlocale(LC_ALL, 'ja_JP.UTF-8');
         $uploaded_file = $request->file('csv_file');
-        $file_path = $uploaded_file->path($uploaded_file);
-
-        $file = new \SplFileObject($file_path);
-        $file->setFlags(\SplFileObject::READ_CSV);
-        $file->setCsvControl("\t");
-
-        $i = 0;
-        $row_count = 0;
-        $data = array();
-        $column_names = [];
-        $registration_csv_list = [];
-        foreach ($file as $row) {
-            // ヘッダー処理
-            if ($row_count === 0) {
-                $headers = $row;
-                foreach ($headers as $header) {
-                    // booksテーブルのカラムと一致する列名のみ取り出す
-                    $result = Book::retrieveBookColumnsByValue($header, 'SJIS-win');
-                    $column_names[] = $result;
-                }
-            }
-            // データ処理
-            if ($row_count > 0) {
-                $data = $row;
-                for ($c = 0; $c < count($data); $c++) {
-                    if (!is_null($column_names[$c])) {
-                        $registration_csv_list[$i][$column_names[$c]] = $data[$c];
-                    }
-                }
-                $i++;
-            }
-            $row_count++;
-        }
-        // DB登録
-        foreach ($registration_csv_list as $key => $value) {
-            $book = new Book();
-            $book->fill($value);
-            $book->save();
-        }
+        $booksData = $this->csvImportService->CsvConvertToArray($uploaded_file);
+        $this->bookService->registerBooks($booksData);
 
         return redirect(route('admin.form'))->with([
             'message' => 'CSV登録が完了しました。',
-            'regist_book_count' => count($registration_csv_list)
+            'regist_book_count' => count($booksData)
         ]);
     }
 }
