ac2d2ef4468161cfed450656c101162441f931a3
t.moriyama <t.moriyama@tcdigital.jp>
2021-11-02 13:06:50 +0900

    環境 環境構築時のエラー解消_composer versionを1へ

--- env/docker/web/etc/init.sh
+++ env/docker/web/etc/init.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 DATE=`date "+%Y-%m-%d"`
-sudo -u nginx touch /var/log/nginx/access.log
-sudo -u nginx touch /var/log/nginx/error.log
-touch /var/log/php-fpm/error.log
-sudo -u nginx touch /var/www/html/storage/logs/laravel-${DATE}.log
-mkdir -p /var/www/html/storage/logs/batch && sudo -u nginx touch /var/www/html/storage/logs/batch/batch-${DATE}.log
+# access.log
+touch /var/log/nginx/access.log
+chown nginx:nginx /var/log/nginx/access.log
+# error.log
+touch /var/log/nginx/error.log
+chown nginx:nginx /var/log/nginx/error.log
 
+touch /var/log/php-fpm/error.log
+# laravel-${DATE}.log
+touch /var/www/html/storage/logs/laravel-${DATE}.log
+chown nginx:nginx /var/www/html/storage/logs/laravel-${DATE}.log
+
+composer self-update --1 # composerのバージョンを1下げる
 composer install
-[ "$(php artisan migrate:status)" = 'No migrations found.' ] && php artisan migrate:refresh --seed && php artisan db:seed --class=TestDataSeeder
+[ "$(php artisan migrate:status)" = 'No migrations found.' ] && php artisan migrate:refresh --seed && php artisan db:seed --class=DatabaseSeeder
 
 exit 0
